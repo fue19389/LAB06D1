0e67f579f902cc4bca93c3c82b9d3ce13f5f6978
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-09-05 23:06:18 -0600

    Modulos completos, beta

--- dflipflop.v
+++ dflipflop.v
@@ -4,7 +4,9 @@
 
 //Modulo flipflop tipo d
 
+//NO ES MUCHO PERO ES TRABAJO HONESTO
 
+//MODULO FLIPFLP DE 4 BITS
 module fliflop4b(input clk, reset, input[3:0]d, output[3:0]q0);
 
   always@(posedge clk, posedge reset)
@@ -12,14 +14,18 @@ module fliflop4b(input clk, reset, input[3:0]d, output[3:0]q0);
     else       q <= d;
 endmodule
 
-module flipflop1b(input clk, reset, Sf, ouput S);
+
+//MODULO FLIPFLOP DE 1 BIT, PARA CALCAR EL EJERCICIO EN CIRCUITVERSE
+module flipflop1b(input clk, reset, S, ouput Sf);
 
   always@(posedge clk, posedge reset)
     if (reset) Sf<=1'b0;
     else       Sf<=S;
 endmodule
 
-module ejer1(input[1:0]SAB, input clk, reset,  output Y, S0, S1, output[1:0]S);
+
+//MODULO DEL EJERCICIO NO.1
+module ejer1(input clk, reset,  output Y, S0, S1, output[1:0]S, SAB);
 
   wire Y, S0, S1;
   reg[1:0] SAB=0;
@@ -32,18 +38,22 @@ module ejer1(input[1:0]SAB, input clk, reset,  output Y, S0, S1, output[1:0]S);
 
 endmodule
 
-module ejer3(input[2:0]SABC, input clk, reset, output, S0, S1, S2, output[2:0]Y);
 
-  wire[2:0] Y;
+// MODULO DEL EJERCICIO NO.2
+module ejer3(input clk, reset, output, S0, S1, S2, A, output[2:0]Y, S);
+
+  wire[2:0] Y, S;
   wire S0, S1, S2;
-  reg[2:0] SABC=0;
+  reg A=0;
 
   assign Y[2] = S2;
   assign Y[1] = (~S2 & S1) | (~S1 & S2);
   assign Y[0] = (~S1 & S0) | (~S0 & S1);
-
-  flipflop1b E2(clk, reset, SABC[0], S0);
-  flipflop1b E3(clk, reset, SABC[1], S1);
-  flipflop1b E4(clk, reset, SABC[2], S2);
+  assign S[2] = (S2 & S1 & S0 & ~A)|(~S2 & ~S1 & ~S0 & ~A)|(S2 & S0 & ~A)|(S2 & ~S1 & A)|(S2 $ S1 & ~S0);
+  assign S[1] = (S1 & S0 & ~A)|(~S1 & S0 & A)|(~S1 & ~S0 & A)|(S1 & ~S0 & A);
+  assign S[0] = ~S0;
+  flipflop1b E2(clk, reset, S[0], S0);
+  flipflop1b E3(clk, reset, S[1], S1);
+  flipflop1b E4(clk, reset, S[2], S2);
 
 endmodule
